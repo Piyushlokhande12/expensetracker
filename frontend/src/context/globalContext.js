11507caa1fc2d03567b3a23fdc3de22339fa6a1a
PIYUSH KUMAR LOKHANDE <134611060+Piyushlokhande12@users.noreply.github.com>
2025-07-24 17:40:18 +0530

    Update globalContext.js

--- frontend/src/context/globalContext.js
+++ frontend/src/context/globalContext.js
@@ -2,7 +2,7 @@ import React, { useContext, useState } from "react"
 import axios from 'axios'
 
 
-const BASE_URL = process.env.REACT_APP_BASE_UR;
+const BAE_URL = process.env.REACT_APP_BASE_URL;
 
 
 const GlobalContext = React.createContext()
